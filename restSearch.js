0710a89905eadeb7ddddfde4492da15a76076043
Reinaldo <reinaldoacdc@gmail.com>
2019-06-05 09:06:45 -0300

    ajuste na consulta da pagina

--- restSearch.js
+++ restSearch.js
@@ -1,23 +1,10 @@
-$('#submit').click(function() {
-    $.ajax({
-        url: 'http://127.0.0.1:5002/sugestions/',
-        type: 'POST',
-        data: {
-            text: $('input[name="why"]').val()
-        },
-        success: function(msg) {
-            alert('OK');
-        }               
-    });
-});
-
 $(document).ready(function() {
     
     var autocomplete = $( "#restSearch" ).autocomplete({
         minLength: 2,
         source: function (request, response) {
             var term = request.term;
-            var restUrl = 'https://autocomplete-dito.herokuapp.com/sugestions/'+term;
+            var restUrl = 'https://autocomplete-dito.herokuapp.com/sugestions?text='+term;
 
             $.getJSON(restUrl, function (data) {
                 var items = [];
@@ -32,4 +19,7 @@ $(document).ready(function() {
 
         appendTo: $('#menu-container')
     })
+
+
+
 });
